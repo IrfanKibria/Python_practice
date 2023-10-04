b88d7b3fbc66824eed7fc610483e7641f61897e7
IrfanKibria <irfan.py3@gmail.com>
2023-10-04 10:54:41 +0600

    Practicing String sliceing and depth

---
+++ string_prac.py
@@ -0,0 +1,24 @@
+name = "Irfan Kibria Talha"
+
+length = len(name)
+
+print(length)
+
+print(f"My name has {length} character")
+
+print(f"My first name is {name[0:5]}")
+
+print(name[-12:-8])
+
+
+
+p = input("Enter Your Name: ")
+
+print(p)
+
+len2= len(p)
+print(f"my name has {len2} charactar")
+print(f"My first name is {p[0:5]}")
+
+print(f"sliceing the name {p[6:15]}")
+print(f"sliceing negativly {p[-12:-6]}")
