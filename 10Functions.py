0730702a30f43557fa8adf07287e17854a9392df
IrfanKibria <irfan.py3@gmail.com>
2024-01-06 14:31:07 +0600

    Function more prac

--- 10Functions.py
+++ 10Functions.py
@@ -26,3 +26,25 @@ def FindMax(a, b):
 char1 = int(input("Enter First Number: "))
 char2 = int(input("Enter First Number: "))
 FindMax(char1, char2)
+
+
+a = 8
+b = 10
+FindMax(a, b)
+
+
+'''find avarage of numbers. we use return for if we want to store the result in a variable then we 
+have to use return funtion. return use for that function have to retun someting when the function is called.
+we use * for the arguments get into a tupple. and use ** for dict'''
+
+def avg(*numbers):
+    sum = 0
+    print(type(numbers))
+    for char in numbers:
+        sum = sum +char
+    return sum/len(numbers)
+    # print(f"Avarage is {sum/len(numbers)}")
+
+
+c = avg(6,5,3,6,3,5,7,100)
+print(c)
