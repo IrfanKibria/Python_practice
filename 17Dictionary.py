f3e4ff89377b51ae914516903c9f9adba354ffcd
irfankibria <irfan.py3@gmail.com>
2024-01-24 13:58:08 +0600

    Dictionary

---
+++ 17Dictionary.py
@@ -0,0 +1,9 @@
+'''
+1. Dictionary are using key value pairs to store values
+2. They are made by using s1={ "key": "value", "key": "value" } syntax
+3. you can get all keys of dictionary by s1. keys( )
+4. you can get all values of dictionary by using s1.values( )
+5. you can get all pairs of dictionary by s1.items( )
+6. you can get the value of an item by using s1['key'] and it will return the value. s1.get("key") will also return 
+the value but it wont throw an error in case there is no value/key available in the dictionary
+'''
