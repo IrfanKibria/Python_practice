d3216266076cce849de819c74490155c2def778e
IrfanKibria <irfan.py3@gmail.com>
2024-01-30 21:57:05 +0600

    Raise Error

---
+++ 20RaiseError.py
@@ -0,0 +1,9 @@
+'''we learnt about the raising custom errors using raise keyword. Sometimes, we want to impose hard and fast 
+regulations so, we can use customer errors if something violates those regulations so, that it does not create any 
+problem later in the program execution and that error can be handled at that point of time only. 
+
+In the previous Lecture, we learnt about how to handle errors using exception handling with try, except and finally 
+blocks. We can also make our custom error classes using any base case error with our defined set of rules to raise 
+that error whenever it violates those rules. '''
+
+
