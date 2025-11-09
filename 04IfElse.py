eb5a83eaffa366f88efc6886e8e98db4d5e4a18a
IrfanKibria <irfan.py3@gmail.com>
2025-11-09 10:33:03 +0600

    Having some conditional problem solving for practice

--- 04IfElse.py
+++ 04IfElse.py
@@ -1,3 +1,5 @@
+'''Driving test problem. If Driver age is less than 18 or age more than 70 then they cannot dirive'''
+
 Age = int(input('Enter Your age: '))
 print("Input age is: ",Age)
 
@@ -9,3 +11,49 @@ elif Age>70:
 
 else:
     print("You can not Drive. Below aged")
+
+
+'''Write a program that takes an integer and prints whether it is even or odd.'''
+
+'''Input a number and check if it’s positive, negative, or zero.'''
+
+'''Input two numbers and print which one is larger (or if they’re equal).'''
+
+
+'''Input a student’s marks and print their grade using these rules:80–100: A+, 70–79: A, 60–69: A−, 50–59: B
+Below 50: Fail
+'''
+
+
+'''Input a year and check if it’s a leap year or not.'''
+
+
+'''Input a letter and determine whether it is a vowel or consonant.'''
+
+
+'''Electricity Bill Calculator
+Given units consumed, calculate total bill:
+First 100 units → 5 taka/unit
+Next 100 units → 8 taka/unit
+Beyond 200 units → 12 taka/unit
+'''
+
+
+'''Triangle Type Checker
+Input three sides of a triangle and determine if it’s:
+Equilateral
+Isosceles
+Scalene
+Or invalid (not a triangle)
+'''
+
+
+'''Salary Bonus
+If employee’s years of service > 5, give a 10% bonus; otherwise, no bonus.
+'''
+
+
+'''Number Divisibility Test
+Check whether a number is divisible by both 3 and 5, only 3, only 5, or neither.
+'''
+
