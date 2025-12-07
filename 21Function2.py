c27ac0a56beae187335c7db42624233039e71657
IrfanKibria <irfan.py3@gmail.com>
2025-12-07 09:53:51 +0600

    Function

--- 21Function2.py
+++ 21Function2.py
@@ -1,10 +1,10 @@
 # Write a recursion function which will return factorial of a number 
-def f(n):
+def factorial(n):
     if n == 0:
         return 1
-    return n* f(n - 1)
+    return n* factorial(n - 1)
 
-print(f(8))
+print(factorial(8))
 
 
 
